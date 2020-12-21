37ddc81c98d4d7623b905ba4c161b2915cd4542b
daniel <dancwill27@gmail.com>
2020-12-21 18:25:02 +1030

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+app.use(cors());
 
-export default App;
+app.get("/", (req, res) => {
+  res.send({ message: "test" });
+});
+
+app.listen(4000);
---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TestText</h1>
+        </header>
+        <p className="App-intro">
+          TestText
+        </p>
+      </div>
+    );
+  }
+
+
+}
+
+export default App;
