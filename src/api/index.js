e5318190bd3ed943276b49fcabaa848e7554bcc1
daniel <dancwill27@gmail.com>
2020-12-22 14:50:46 +1030

    hero pictures up

--- src/api/index.js
+++ src/api/index.js
@@ -10,6 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/test",testRouter);
 
 app.listen(4000, () => {
--- src/client/components/HeroTile.js
+++ src/client/components/HeroTile.js
@@ -11,25 +11,73 @@ class HeroTile extends Component {
     };
   }
 
-  getWinrate() {
-    var url = "https://api.opendota.com/api/heroes/1/matchups";
-    axios.get(url).then((result) => {
-      this.setState({
-        winrate: 2,
-      });
-    });
+  async getWinrate() {
+    var url =
+      "https://api.opendota.com/api/heroes/" + this.state.id + "/durations";
+    await axios.get(url).then((result) => {});
   }
 
-  componentDidMount() {
-    // this.getWinrate();
-    //console.log(this.props)
+  getHeroImg(name) {
+    var temp = name.toLowerCase();
+    var url = "https://api.opendota.com/apps/dota2/images/heroes/";
+    var extension = "_full.png";
+    console.log(temp);
+    switch (temp) {
+      case "anti-mage":
+        return url + "antimage" + extension;
+      case "centaur warrunner":
+        return url + "centaur" + extension;
+      case "clockwerk":
+        return url + "rattletrap" + extension;
+      case "doom":
+        return url + "doombringer" + extension;
+      case "io":
+        return url + "wisp" + extension;
+      case "lifestealer":
+        return url + "life_stealer" + extension;
+      case "magnus":
+        return url + "magnataur" + extension;
+      case "nature's prophet":
+        return url + "furion" + extension;
+      case "necrophos":
+        return url + "necrolyte" + extension;
+      case "outworld devourer":
+        return url + "obsidian_destroyer" + extension;
+      case "queen of pain":
+        return url + "queenofpain" + extension;
+      case "shadow fiend":
+        return url + "nevermore" + extension;
+      case "timbersaw":
+        return url + "shredder" + extension;
+      case "treant protector":
+        return url + "treant" + extension;
+      case "underlord":
+        return url + "abyssal_underlord" + extension;
+      case "vengeful spirit":
+        return url + "vengefulspirit" + extension;
+      case "windranger":
+        return url + "windrunner" + extension;
+      case "wraith king":
+        return url + "skeleton_king" + extension;
+      case "zeus":
+        return url + "zuus" + extension;
+      default:
+        temp = temp.replace(/ /g, "_");
+        return url + temp + extension;
+    }
   }
 
   render() {
-    console.log(this.state.heroes);
+    //this.getWinrate();
     return (
       <tbody>
-        <td>{this.state.heroName}</td>
+        <td>
+          <img
+            class="col-md-2"
+            src={this.getHeroImg(this.state.heroName)}
+          ></img>
+          {this.state.heroName}
+        </td>
         <td>{this.state.id}</td>
       </tbody>
     );
