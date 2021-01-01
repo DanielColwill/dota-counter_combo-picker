e11f55e6da259a5838f1437e459cafe0c3e47f57
daniel <dancwill27@gmail.com>
2021-01-01 18:41:53 +1030

    package lock change

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
-const publicPath = path.join(__dirname, '');
+const publicPath = path.join(__dirname, "");
 
-require('dotenv').config();
+require("dotenv").config();
 
 var testRouter = require("./routes/test");
 var heroesRouter = require("./routes/heroes");
@@ -13,19 +13,30 @@ var matchupRouter = require("./routes/matchups");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use("/test",testRouter);
-app.use("/",heroesRouter);
-app.use("/",winrateRouter);
-app.use("/",matchupRouter);
+app.use(express.static(__dirname + '/public'));
+
+if (process.env.NODE_ENV === "production"){
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+  });
+}
+
+app.use("/test", testRouter);
+app.use("/", heroesRouter);
+app.use("/", winrateRouter);
+app.use("/", matchupRouter);
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log('Server is up!');
+  console.log("Server is up!");
 });
