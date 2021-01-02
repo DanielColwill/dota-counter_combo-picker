b1384c31923e8dd50c7854fe14742f2988ff6a0f
daniel <dancwill27@gmail.com>
2021-01-02 18:43:44 +1030

    changed cors whitelist

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
-const publicPath = path.join(__dirname, "");
 
 require("dotenv").config();
 
@@ -14,28 +13,39 @@ var matchupRouter = require("./routes/matchups");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.use("/test", testRouter);
 app.use("/", heroesRouter);
 app.use("/", winrateRouter);
 app.use("/", matchupRouter);
 
-const whitelist = ["http://localhost:3000","http://localhost:4000"]
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:4000",
+  "https://dota-app-combo-counter.herokuapp.com",
+];
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
+    console.log("** Origin of request " + origin);
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
+      console.log("Origin acceptable");
+      callback(null, true);
     } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
-app.use(cors(corsOptions))
+  },
+};
+app.use(cors(corsOptions));
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Server is up!");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
